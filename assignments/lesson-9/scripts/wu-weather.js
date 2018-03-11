95b9d56cd671a953d466c88d3dc26f8dcabd7444
ChelPark <chelseabpark@icloud.com>
2018-03-11 18:04:15 +1300

    forecast json

--- assignments/lesson-9/scripts/wu-weather.js
+++ assignments/lesson-9/scripts/wu-weather.js
@@ -8,11 +8,15 @@ weatherObject.onload = function() {
     var weatherInfo = JSON.parse(weatherObject.responseText);
     console.log(weatherInfo);
     
-    document.getElementById('place').innerHtml = weatherInfo.current_observation.display_location.full;
+    document.getElementById('conditions').innerHtml = weatherInfo.current_observation.weather;
     
     document.getElementById('currentTemp').innerHtml = weatherInfo.current_observation.temp_f;
     
     document.getElementById('weather_icon').innerHtml = weatherInfo.current_observation.icon_url;
+    
+    document.getElementById('windspeed').innerHtml = weatherInfo.current_observation.wind_mph;
+    
+    document.getElementById('textForecast').innerHtml = weatherInfo.current_observation.txt_forecast: forecastday.fctext;
     
     
 } //end of onload
